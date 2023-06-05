770ff80170cd020515c573951d4fb1d75c46534c
KyojuroB <grizzlypandastudio@gmail.com>
2023-06-05 14:57:28 -0700

    b.v.idk.idk.idk

---
+++ Island-Escape-GP/Assets/Scripts/DigitalClock.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+public class DigitalClock : MonoBehaviour
+{
+
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      int hours =  GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().GetHours();
+      int minutes = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().GetMinutes();
+      string apm;
+        if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().isAmPm == true)
+        {
+            apm = "Am";
+        }
+        else
+        { 
+         apm = "Pm";
+        }
+        gameObject.GetComponent<TextMeshProUGUI>().text = (hours.ToString() + ":" + minutes.ToString() + "   " + apm);
+    }
+}
